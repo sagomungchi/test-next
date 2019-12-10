4ee4bebf17917986d53a4d5b8c753aea3b05d020
Jowooseok <whdntjrsla12@naver.com>
2019-12-11 04:14:07 +0900

    Video Uplode page complete

---
+++ pages/howto.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Player } from 'video-react';
+// nodejs library that concatenates classes
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+// core components
+import styles from "assets/jss/nextjs-material-kit/pages/components.js";
+import "node_modules/video-react/dist/video-react.css"; // import css
+
+const useStyles = makeStyles(styles);
+
+const Howto = () => {
+  const classes = useStyles();
+
+
+
+  return (
+    <div>
+      <div style={{ width: "100%", height: "880px" , paddingTop: 50 }}>
+       <div style={{ margin: "auto" , marginTop:"75px",width:"70%"}}>
+        <Player
+          playsInline
+          poster="../static/1.jpg"
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Howto;
